d235dc10ac4940311103fcccbc62b310421acdba
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2016-04-10 13:29:13 -0600

    Etiquetas y Texto

---
+++ EtiquetasCuadrosDeTexto/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbldigit = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lbldigit
+        '
+        Me.lbldigit.AutoSize = True
+        Me.lbldigit.Location = New System.Drawing.Point(48, 48)
+        Me.lbldigit.Name = "lbldigit"
+        Me.lbldigit.Size = New System.Drawing.Size(61, 13)
+        Me.lbldigit.TabIndex = 0
+        Me.lbldigit.Text = "Digit a Text"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(139, 48)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(133, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "Hola"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lbldigit)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbldigit As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
