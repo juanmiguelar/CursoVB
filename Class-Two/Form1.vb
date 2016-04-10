38a5d3edcb4d10d1ecbd34bead8eace2b0f85748
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2016-04-09 21:41:47 -0600

    Clase de tipos de datos

---
+++ Class-Two/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim number As Integer = 1
+        MsgBox(number)
+
+        Dim numberD As Double = 123.123
+        MsgBox(numberD)
+
+        Dim txt As String = "Hola"
+        MsgBox(txt)
+
+        Dim today As Date = "09-04-16"
+        MsgBox(today)
+
+        today = "09-JAN-2016"
+        MsgBox(today)
+    End Sub
+End Class
