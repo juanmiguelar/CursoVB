9b001b1c0b2f346f81820d898cdacc2e5292fa6e
Juan Miguel <juan.ariasmejias@ucr.ac.cr>
2016-04-10 14:38:53 -0600

    TabControl

---
+++ TabControl/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.lblNombre = New System.Windows.Forms.Label()
+        Me.lblIdInsertar = New System.Windows.Forms.Label()
+        Me.btnInsertar = New System.Windows.Forms.Button()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.lblIDEliminar = New System.Windows.Forms.Label()
+        Me.txtIDEliminar = New System.Windows.Forms.TextBox()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(13, 13)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(259, 236)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.Silver
+        Me.TabPage1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.TabPage1.Controls.Add(Me.txtID)
+        Me.TabPage1.Controls.Add(Me.txtNombre)
+        Me.TabPage1.Controls.Add(Me.btnInsertar)
+        Me.TabPage1.Controls.Add(Me.lblIdInsertar)
+        Me.TabPage1.Controls.Add(Me.lblNombre)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(251, 210)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Insertar"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.btnEliminar)
+        Me.TabPage2.Controls.Add(Me.txtIDEliminar)
+        Me.TabPage2.Controls.Add(Me.lblIDEliminar)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(251, 210)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Eliminar"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.Button1)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(251, 210)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Actualizar"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'lblNombre
+        '
+        Me.lblNombre.AutoSize = True
+        Me.lblNombre.Location = New System.Drawing.Point(24, 44)
+        Me.lblNombre.Name = "lblNombre"
+        Me.lblNombre.Size = New System.Drawing.Size(44, 13)
+        Me.lblNombre.TabIndex = 0
+        Me.lblNombre.Text = "Nombre"
+        '
+        'lblIdInsertar
+        '
+        Me.lblIdInsertar.AutoSize = True
+        Me.lblIdInsertar.Location = New System.Drawing.Point(24, 81)
+        Me.lblIdInsertar.Name = "lblIdInsertar"
+        Me.lblIdInsertar.Size = New System.Drawing.Size(16, 13)
+        Me.lblIdInsertar.TabIndex = 1
+        Me.lblIdInsertar.Text = "Id"
+        '
+        'btnInsertar
+        '
+        Me.btnInsertar.Location = New System.Drawing.Point(145, 163)
+        Me.btnInsertar.Name = "btnInsertar"
+        Me.btnInsertar.Size = New System.Drawing.Size(75, 23)
+        Me.btnInsertar.TabIndex = 2
+        Me.btnInsertar.Text = "Insertar"
+        Me.btnInsertar.UseVisualStyleBackColor = True
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(136, 37)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(100, 20)
+        Me.txtNombre.TabIndex = 3
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(136, 73)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(100, 20)
+        Me.txtID.TabIndex = 4
+        '
+        'lblIDEliminar
+        '
+        Me.lblIDEliminar.AutoSize = True
+        Me.lblIDEliminar.Location = New System.Drawing.Point(34, 67)
+        Me.lblIDEliminar.Name = "lblIDEliminar"
+        Me.lblIDEliminar.Size = New System.Drawing.Size(16, 13)
+        Me.lblIDEliminar.TabIndex = 2
+        Me.lblIDEliminar.Text = "Id"
+        '
+        'txtIDEliminar
+        '
+        Me.txtIDEliminar.Location = New System.Drawing.Point(120, 60)
+        Me.txtIDEliminar.Name = "txtIDEliminar"
+        Me.txtIDEliminar.Size = New System.Drawing.Size(100, 20)
+        Me.txtIDEliminar.TabIndex = 3
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(145, 174)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
+        Me.btnEliminar.TabIndex = 4
+        Me.btnEliminar.Text = "Eliminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(150, 169)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.TabPage3.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents btnInsertar As Button
+    Friend WithEvents lblIdInsertar As Label
+    Friend WithEvents lblNombre As Label
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents lblIDEliminar As Label
+    Friend WithEvents btnEliminar As Button
+    Friend WithEvents txtIDEliminar As TextBox
+    Friend WithEvents Button1 As Button
+End Class
